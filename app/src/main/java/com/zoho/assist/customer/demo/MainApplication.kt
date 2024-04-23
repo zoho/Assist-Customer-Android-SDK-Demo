8ab7ac15ca59cdf56ee35f134125eddcc3739b3a
ajith-14314 <ajithmurugan@zohocorp.com>
2024-04-23 12:54:32 +0530

    Revert "sdk token added"
    This reverts commit 19f5a868a68d9689e0119ccc89254940048b9432.

--- app/src/main/java/com/zoho/assist/customer/demo/MainApplication.kt
+++ app/src/main/java/com/zoho/assist/customer/demo/MainApplication.kt
@@ -9,7 +9,7 @@ class MainApplication : AssistSDKApplication() {
     override fun onCreate() {
         super.onCreate()
         AssistSession.INSTANCE.setContext(this)
-        AssistSession.INSTANCE.setAuthToken("OlyO55SENBUmnpF1K5fIjveyz0mjpadfj3ZAbq2whmDClHG3AB31SsNVM36eOZhzzP4coagKx/2gH5XfynmR5pqnVbqk/o0KeItwz2yAguFPVDkeB5ZbSGcuf7P6upypJUA=") // Set your auth token here to enable some features
+        AssistSession.INSTANCE.setAuthToken("") // Set your auth token here to enable some features
         AssistSession.INSTANCE.enableServiceQueue(this,true)
 
     }
