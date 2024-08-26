e1181e8da92fde4e2ee59677e8ddf91b7c911300
ajith-14314 <ajithmurugan@zohocorp.com>
2024-08-26 13:04:56 +0530

    our auth token removed

--- app/src/main/java/com/zoho/assist/customer/demo/MainApplication.kt
+++ app/src/main/java/com/zoho/assist/customer/demo/MainApplication.kt
@@ -9,7 +9,7 @@ class MainApplication : AssistSDKApplication() {
     override fun onCreate() {
         super.onCreate()
         AssistSession.INSTANCE.setContext(this)
-        AssistSession.INSTANCE.setAuthToken("wSsVR60n+hf1Ca8ozjSrde47yA5QB1v/EEV42FH16SX9F6vC8cc5lEGfDFOgTaMYEWdsQGZHprh8kRYD1DcIiNotzVlSDyiF9mqRe1U4J3x1pLnvkT7OV21dkxOIKI4PwQ9vmA==") // Set your auth token here to enable some features
+        AssistSession.INSTANCE.setAuthToken("") // Set your auth token here to enable some features
 
 
     }
